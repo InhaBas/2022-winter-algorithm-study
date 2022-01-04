68190d7d2b3df0e212a8cd412d4a12b7e79543cf
Dong-Hyeon-Yu <81698076+Dong-Hyeon-Yu@users.noreply.github.com>
2022-01-04 22:01:02 +0900

    신규 아이디 추천 두번째 풀이
    다른 사람 풀이를 참고해서 재작성.
    
    첫번째 풀 때는 id 길이가 1000까지 인 것을 보고, O(n^2) 까지도 충분하겠다고 생각했음. 
    문제를 읽어보니 O(n)을 7번 반복하면 되는 문제라 어떻게 풀어도 풀리겠구나 빨리만 풀면 되겠다는 생각으로 접근.
    바로 생각나는 방법이 vector 여서 vector를 일단 여러개 만들어놓고 시작했다.
    
    이번학기에 c 코딩만 열심히 해서 그런지 string 타입이 있다는 걸 잊어버렸다. 
    문자열이 +연산자로 덧셈이 된다는 걸 까먹고있었다. (c는 안된다...)
    다른 사람이 푼 풀이를 보고 아차 싶어 string으로 접근했는데,
    이미 그 사람껄 봐서 그런지 비슷하게 흘러간 느낌이다.

--- level1/신규 아이디 추천.cpp
+++ level1/신규 아이디 추천.cpp
@@ -1,68 +1,62 @@
 #include <string>
-#include <vector>
 #include <cctype>
 
 using namespace std;
 
 bool isAcceptable(char a) {
-    return a == '-' || a == '_' || a == '.' || isdigit(a) || islower(a) || isupper(a);
+    return a == '-' || a == '_' || a == '.' || isdigit(a) || islower(a);
 }
 
 string solution(string new_id) {
-    vector<vector<char>> answer(4);
     
     // 1st
-    for(int i = 0; i < new_id.length(); i++){
-        char cur_char = new_id[i];
-        
-        if(isupper(cur_char)){
-            answer[1].push_back(tolower(cur_char));
-        }
-        else {
-            answer[1].push_back(cur_char);
-        }
-    } 
+    for(auto &ch: new_id) 
+        if(isupper(ch)) ch |= 32;
+    
     // 2nd
-    for(int i = 0; i < answer[1].size(); i++){
-        char cur_char = answer[1][i];
-        
-        if(isAcceptable(cur_char)){
-            answer[2].push_back(cur_char);
-        }
-    } 
+    string ans;
+    for (auto &ch: new_id)
+        if (isAcceptable(ch)) ans += ch;
+    
+    
+    new_id = ans;
+    ans = "";
+    
     //3rd
     bool continuousDotFlag = false;
-    for(int i = 0; i < answer[2].size(); i++){
-        char cur_char = answer[2][i];
-        if (cur_char == '.' && !continuousDotFlag) { //first dot
-            if (i != 0) answer[3].push_back(cur_char);
-            continuousDotFlag = true;
+    if(!new_id.empty())
+        for(auto &ch: new_id){
+            if (ch == '.' && !continuousDotFlag) {
+                continuousDotFlag = true;
+                ans += ch;
+            }
+            else if (ch != '.') {
+                ans += ch;
+                continuousDotFlag = false;
+            }
         }
-        if (cur_char != '.'){
-            continuousDotFlag = false;
-            answer[3].push_back(cur_char);
-        }
-    } 
+    
     //4th
-    if (!answer[3].empty() && answer[3].back() == '.') 
-        answer[3].pop_back();
+    if (ans.front() == '.') ans.erase(ans.begin());
+    if (ans.back() == '.') ans.pop_back();
+        
     
     //5th
-    if (answer[3].empty())
-        answer[3].push_back('a');
+    if (ans.empty())
+        ans = "a";
     
     //6th
-    if (answer[3].size() >= 16) {
-        answer[3] = vector<char>(answer[3].begin(), answer[3].begin() + 15);
+    if (ans.length() >= 16) {
+        ans = ans.substr(0, 15);
     }
-    if (answer[3].back() == '.')
-        answer[3].pop_back();
+    if (ans.back() == '.')
+        ans.pop_back();
     
     //7th
-    char last = answer[3].back();
-    while(answer[3].size() < 3){
-        answer[3].push_back(last);
+    char last = ans.back();
+    while(ans.length() < 3){
+        ans += last;
     }
     
-    return string(answer[3].begin(), answer[3].end());
+    return ans;
 }
