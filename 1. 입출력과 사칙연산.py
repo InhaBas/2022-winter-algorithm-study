8e2bbffe7406edcf16f2428fc0320e2df0e4661b
shs1018 <1018gustj@naver.com>
2022-01-05 09:50:03 +0900

    tex

---
+++ 1. 입출력과 사칙연산.py
@@ -0,0 +1,61 @@
+next = '_'*20
+print(next)
+#1.
+print("Hello World!")
+print(next)
+#2.
+print("강한친구 대한육군 \n강한친구 대한육군")
+print(next)
+#3.
+print("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|")
+print(next)
+#4.
+print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\'^\'\'    |\n||_/=\\\\__|")
+print(next)
+#5.
+A = 1
+B = 2
+print(A+B)
+print(next)
+#6.
+A = 3
+B = 2
+print(A-B)
+print(next)
+#7.
+A = 1
+B = 2
+print(A*B)
+print(next)
+#8.
+A = 1
+B = 3
+print(A/B)
+print(next)
+#9.
+A = 7
+B = 3
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+print(next)
+#10.
+A = 5
+B = 8
+C = 4
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print((A%C)*(B%C)%C)
+print(next)
+#11.
+A = 472
+B = 385
+three = A*(B%10)
+four = A*((B%100)//10)
+five = A*(B//100)
+six = A*B
+print(three, four, five, six, sep='\n')
+print(next)
