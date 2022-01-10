35864dcc9f18c2c60cf10775cf107b9cbf5db8a2
shs1018 <1018gustj@naver.com>
2022-01-11 00:12:23 +0900

    변수를 소문자로 바꾸고, 이항연산자의 간격을 띄웠습니다.

--- baekjoon_2.py
+++ baekjoon_2.py
@@ -1,43 +1,43 @@
-#1
+# 1
 
-A = int(input())
+a = int(input())
 
-B = int(input())
+b = int(input())
 
-if A > B:
+if a > b:
     print('>')
-elif A == B:
+elif a == b:
     print('==')
-elif A < B:
+elif a < b:
     print('<')
 
-#2
+# 2
 
-A = int(input('test result:'))
+a = int(input('test result:'))
 
-if 100 >= A >= 90:
+if 100 >= a >= 90:
     print('A')
-elif 90 > A >= 80:
+elif 90 > a >= 80:
     print('B')
-elif 80 > A >= 70:
+elif 80 > a >= 70:
     print('C')
-elif 70 > A >= 60:
+elif 70 > a >= 60:
     print('D')
 else:
     print('F')
 
-#3
+# 3
 
-A = int(input())
+a = int(input())
 
-if A // 4 == 0 and A % 100 !=0:
+if a // 4 == 0 and a % 100 != 0:
     print('1')
-elif A // 400 == 0:
+elif a // 400 == 0:
     print('1')
 else:
     print('0')
 
-#4
+# 4
 
 x, y = map(int, input().split())
 # 솔직히 map 함수의 사용법을 아직 잘 모르겠습니다. int(input())을 x,y에 두 줄에 걸쳐 할당하는게 더 직관적이긴 하나 map 함수를 이용하는게 더 깔끔해 보입니다.
@@ -51,13 +51,13 @@ elif x < 0 and y < 0:
 else:
     print('4')
 
-#5
+# 5
 
-H, M = map(int, input().split())
+h, m = map(int, input().split())
 
-if M >= 45:
+if m >= 45:
     print(H, M + 15)
-elif H == 0:
+elif h == 0:
     print(23, M + 15)
 else:
-    print(H-1, M + 15)
+    print(H - 1, M + 15)
