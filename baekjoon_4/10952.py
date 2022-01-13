9720c1af22c42ec9f23d997b53d36a9f2e0d9ae0
shs1018 <1018gustj@naver.com>
2022-01-13 16:07:34 +0900

    while 문 풀이입니다_2

--- baekjoon_4/10952.py
+++ baekjoon_4/10952.py
@@ -9,4 +9,4 @@ while True:
     if a == 0 and b == 0:
         break
 
-    print(f"{a} + {b} = {a + b}")  # f-string을 통해 문자열을 만들었습니다. 좀 더 익숙해지도록 하겠습니다.
+    print(f"{a} + {b} = {a + b}")  # f-string을 통해 문자열을 만들었습니다. 좀 더 익숙해지도록 하겠습니다!
