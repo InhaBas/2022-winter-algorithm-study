ea443bae50d952518bbd910c599bd836795f3466
Dong-Hyeon-Yu <81698076+Dong-Hyeon-Yu@users.noreply.github.com>
2022-01-13 21:28:01 +0900

    Update 뉴스 클러스터링.cpp
    transform, for_each, generate 비슷함 함수들이네용

--- level2/뉴스 클러스터링.cpp
+++ level2/뉴스 클러스터링.cpp
@@ -1,21 +1,33 @@
 #include <string>
 #include <cctype>
+#include <algorithm>
 #include <map>
 #define mySet map<string, int>
 
 using namespace std;
 
 map<string, int> makeSet(string& str) {
+    
+    /* transform 
+    : container 요소를 순차적으로 접근, 
+      해당 요소에 인자로 넘긴 함수를 적용, (함수 포인터, 람다도 가능)
+      3번째 인자로 넘긴 container에 대입
+      (내부적으로 대입 연산자를 이용하기 때문에, 미리 메모리 할당이 되어있지 않으면 오류발생. 
+      back_inserter()를 이용하면 push_back 으로 넣어줌.)
+      
+      ex) 
+         string src = "~~~생략~~~";
+         string dst;
+         
+         transform(src.begin(), src.end(), back_inserter(dst.begin()), [](char c) {return toupper(c);});
+    */
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    
     mySet set;
     for (int i = 0; i < str.length()-1; i++) {
         string ss = str.substr(i, 2);
 
-        if (isalpha(ss[0]) && isalpha(ss[1])) { /* c++ 은 이런 부분이 화가 납니다.. ㅂㄷ */
-            ss[0] = toupper(ss[0]); 
-            ss[1] = toupper(ss[1]);
-            
-            set[ss]++;
-        }
+        if (isalpha(ss[0]) && isalpha(ss[1])) set[ss]++;
     }
     return set;
 }
