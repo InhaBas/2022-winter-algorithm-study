1ea845cbd8524b72aad4b8964632643a871f1dd7
Dong-Hyeon-Yu <81698076+Dong-Hyeon-Yu@users.noreply.github.com>
2022-01-16 22:26:24 +0900

    Update 튜플.cpp
    각각의 숫자를 카운팅하는 방법으로 적용.
    
    알고리즘 하다보면 화가 나는데,, 그럴때면 멍청해지는 듯하다.

--- level2/튜플.cpp
+++ level2/튜플.cpp
@@ -1,62 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <cctype>
-#include <set>
-#define MySubset priority_queue<string, vector<string>, decltype(compare)>
+#include <map>
+#include <algorithm>
+#define pii pair<int, int>
 using namespace std;
 
-auto compare = [](string a, string b) {return a.length() > b.length();};
-
-void splitIntoSubsets(string& s, MySubset& subsetList) {
-    string subset;
-    int pos;
-    while ((pos = s.find("},{")) != string::npos) {
-        subset = s.substr(0, pos);
-        subsetList.push(subset);
-        s.erase(0, pos + 3);
-    }
-    subsetList.push(s);
-}
-
-bool isNewElement(set<int>& tuple, int e) {
-    return tuple.find(e) == tuple.end();
-}
-
 vector<int> solution(string s) {
-    set<int> elementsInTuple;
-    s = s.substr(2, s.length() - 4);
-    MySubset subsetList(compare);
-
-    splitIntoSubsets(s, subsetList);
+    map<string, int> tupleElement; 
     
     vector<int> answer;
-    while (!subsetList.empty()){
-        bool find = false;
-        string num; 
-        string subset = subsetList.top();
-        subsetList.pop();
-    
-        for (auto& ch: subset) {    
-            
-            if (isdigit(ch)) 
-                num += ch;
-            else if (ch == ',') {
-                if (isNewElement(elementsInTuple, stoi(num))) { /* 새로운 요소이면 추가! */
-                    elementsInTuple.insert(stoi(num));
-                    answer.push_back(stoi(num));
-                    find = true;
-                    break;
-                }
-                num = "";
-            }
-        }
+    string num;
+    for (auto& ch: s) {      
+        if (isdigit(ch)) 
+            num += ch;
 
-        if (!find) { /* 부분집합의 마지막 요소는 for문 안에서 검사를 못하기 때문에 여기서 검사 */
-            elementsInTuple.insert(stoi(num));
-            answer.push_back(stoi(num));
+        else if (ch == ',') {
+            tupleElement[num]++;
+            num = "";
         }
     }
+    tupleElement[num]++;
 
+    vector<pii> tmpAnswer;
+    for (auto& [key, value]: tupleElement) tmpAnswer.push_back({value, stoi(key)});
+    sort(tmpAnswer.begin(), tmpAnswer.end(), greater<pii>());
+    
+    for (auto& [value, key]: tmpAnswer) answer.push_back(key);
     return answer;
 }
